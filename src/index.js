82c0b7943db8da97f1a7f6384719f1327d2d5999
Lajanke <l.j.kenny@hotmail.com>
2020-06-10 22:18:34 +0100

    Tests Heroku deploy.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Test</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+//serviceWorker.unregister();
